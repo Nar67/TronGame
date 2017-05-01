491dba516a07bb2a399f70b4278ce3b252470fc2
nar67 <narcis.tg@gmail.com>
2017-05-01 18:37:10 +0200

    Ramongolo that passes the Jutge

--- game/AIRamongolo.cc
+++ game/AIRamongolo.cc
@@ -8,7 +8,7 @@ using namespace std;
  * Write the name of your player and save this file
  * with a name matching the format "AIyourname.cc".
  */
-#define PLAYER_NAME Demo
+#define PLAYER_NAME Ramongolo
 
 
 struct PLAYER_NAME : public Player {
@@ -27,7 +27,38 @@ struct PLAYER_NAME : public Player {
      */     
     //vector<int> my_awesome_vector_of_integers;
 
+    //returns the index of the maximum element in a vector<int>
+    int max_vec(const vector<int>& v){
+    	int max = 0;
+    	for (int i = 0; i < (int)v.size(); ++i) {
+    		if(v[i] > v[max])
+    			max = i;
+    	}
+    	return max;
+    }
 
+    //returns a vector with the id of all empty neighbours of vert.
+    vector<int> find_empty_neighbours(const int& vert){
+    	vector<int> res;
+    	vector<int> nb = vertex(vert).neighbours;
+    	for(int i = 0; i < (int)nb.size(); i++) {
+    		int id = nb[i];
+    		if(vertex(id).wall == -1 and vertex(id).bike == -1)
+    			res.push_back(id);
+    	}
+    	return res;
+    }
+
+    //returns the index for the neighbour with the maximum empty neighbours
+    int max_empty_nb(const vector<int>& empty_nb){
+    	vector<int> res;
+    	for(int i = 0; i < (int)empty_nb.size(); i++){
+    		vector<int> nb = find_empty_neighbours(empty_nb[i]);
+    		res.push_back(nb.size());
+    	}
+    	return max_vec(res);
+
+    }
     /**
      * Play method.
      * 
@@ -36,11 +67,11 @@ struct PLAYER_NAME : public Player {
      * for this round.
      */
     void play () {
-		
         vector<int> my_bikes = bikes(me());
         for (int i = 0; i < (int)my_bikes.size(); ++i) {
 			
 			const Bike& my_bike = bike(my_bikes[i]);
+			const int& my_position = my_bike.vertex;
 			
 			// Do something only if this bike is alive
 			if (!my_bike.alive) {
@@ -52,22 +83,20 @@ struct PLAYER_NAME : public Player {
 				continue;
 			}
 			
-			// Find all empty neighbours
 			vector<int> neighbours = vertex(my_bike.vertex).neighbours;
-			vector<int> empty_neighbours;
-			for (int i = 0; i < (int)neighbours.size(); i++) {
-				int id = neighbours[i];
-				if (vertex(id).wall == -1) {
-					empty_neighbours.push_back(id);
-				}
-			}
-			
+			// Find all empty neighbours
+			vector<int> empty_neighbours = find_empty_neighbours(my_position);
+
+
 			// Create an empty movement
 			Movement movement(my_bike.id);
 			
 			// Set next_vertex to a random empty neighbour, if any, or to a random neighbour otherwise
 			if (!empty_neighbours.empty()) {
-				movement.next_vertex = empty_neighbours[ rand() % (int)empty_neighbours.size() ];
+				int max_nb = max_empty_nb(empty_neighbours);
+				movement.next_vertex = empty_neighbours[max_nb];
+
+				//movement.next_vertex = empty_neighbours[ rand() % (int)empty_neighbours.size() ];
 			} else {
 				movement.next_vertex = neighbours[ rand() % (int)neighbours.size() ];
 			}
